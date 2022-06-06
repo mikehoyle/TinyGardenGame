95bf46c0ae1e2ed53bda8692830dc992980c2644
mikehoyle <michael.a.hoyle@gmail.com>
2022-06-05 23:02:33 -0700

    Replace Extended collisions system with my own

--- TinyGardenGame/Src/Core/Systems/CollisionSystem.cs
+++ TinyGardenGame/Src/Core/Systems/CollisionSystem.cs
@@ -1,12 +1,12 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
 using Microsoft.Xna.Framework;
-using MonoGame.Extended;
-using MonoGame.Extended.Collections;
-using MonoGame.Extended.Collisions;
 using MonoGame.Extended.Entities;
 using MonoGame.Extended.Entities.Systems;
+using QuadTrees;
+using QuadTrees.QTreeRectF;
 using TinyGardenGame.Core.Components;
 using TinyGardenGame.MapGeneration;
+using RectangleF = System.Drawing.RectangleF;
 
 namespace TinyGardenGame.Core.Systems {
   /**
@@ -15,120 +15,131 @@ namespace TinyGardenGame.Core.Systems {
    */
   public class CollisionSystem : EntityUpdateSystem {
     private readonly GameMap _map;
-    private readonly KeyedCollection<int, CollisionActor> _collisionActors;
     private ComponentMapper<PositionComponent> _positionComponentMapper;
     private ComponentMapper<CollisionFootprintComponent> _collisionComponentMapper;
-    private CollisionComponent _collisionEngine;
     private ComponentMapper<MotionComponent> _motionComponentMapper;
+    private readonly QuadTreeRectF<CollidableObject> _collisionQuadTree;
 
-    public CollisionSystem(GameMap map) :
-        base(Aspect.All(typeof(PositionComponent), typeof(CollisionFootprintComponent))) {
+    public CollisionSystem(GameMap map) : base(Aspect.All(
+        typeof(PositionComponent), typeof(CollisionFootprintComponent), typeof(MotionComponent))) {
       _map = map;
-      _collisionActors = new KeyedCollection<int, CollisionActor>(actor => actor.EntityId);
+      _collisionQuadTree = new QuadTreeRectF<CollidableObject>(
+          new RectangleF(_map.Bounds.X, _map.Bounds.Y, _map.Bounds.Width, _map.Bounds.Height));
     }
     
     public override void Initialize(IComponentMapperService mapperService) {
       _positionComponentMapper = mapperService.GetMapper<PositionComponent>();
       _collisionComponentMapper = mapperService.GetMapper<CollisionFootprintComponent>();
       _motionComponentMapper = mapperService.GetMapper<MotionComponent>();
-      // Note: this is not an ECS component, but is named and made to be used as a
-      // monogame game component. We aren't using it that way.
-      _collisionEngine = new CollisionComponent(_map.Bounds);
     }
 
+    // TODO: Handle edge of map collision
+    // This currently only handles collisison of moving entities (such as the player) into
+    // stationary entities. That may need to expand in the future.
     public override void Update(GameTime gameTime) {
       foreach (var entity in ActiveEntities) {
-        if (_collisionActors.TryGetValue(entity, out var actor)) {
-          actor.UpdateBounds(); 
-        } else {
-          AddCollisionActor(entity);
+        var position = _positionComponentMapper.Get(entity);
+        var collisionFootprint = _collisionComponentMapper.Get(entity);
+        var motion = _motionComponentMapper.Get(entity);
+
+        var collisionRect = CollidableObject.GetCollisionRect(
+            position.Position + motion.CurrentMotion, collisionFootprint);
+        var correctionVec = Vector2.Zero;
+        foreach (var collisionTarget in _collisionQuadTree.GetObjects(collisionRect)) {
+          // This is assuming collision targets never overlap, and an entity will only
+          // ever intersect two objects in different axes. It may need revisiting to not
+          // overcorrect if that assumption ever fails.
+          correctionVec += CalculatePenetrationVector(collisionRect, collisionTarget.Rect);
         }
-      }
 
-      // Always check if entities have been removed entirely.
-      // I *really* wish there was a cleaner way to do this (like with events), but any useful
-      // functionality is private in the Extended library.
-      foreach (var removedEntity in _collisionActors.Keys.Except(ActiveEntities)) {
-        RemoveCollisionActor(removedEntity);
+        // To avoid possible overcorrecting, which might create a jitter effect, we
+        // bottom out the correction at zero.
+        motion.CurrentMotion = correctionVec.Length() < motion.CurrentMotion.Length()
+            ? motion.CurrentMotion - correctionVec
+            : Vector2.Zero;
       }
+    }
 
-      _collisionEngine.Update(gameTime);
+    private static Vector2 CalculatePenetrationVector(RectangleF source, RectangleF target) {
+      var intersectingRectangle = new RectangleF(source.X, source.Y, source.Width, source.Height);
+      intersectingRectangle.Intersect(target);
+      
+      if (intersectingRectangle.Width < intersectingRectangle.Height) {
+        var d = RectCenter(source).X < RectCenter(target).X
+            ? intersectingRectangle.Width : -intersectingRectangle.Width;
+        return new Vector2(d, 0);
+      }
+      else {
+        var d = RectCenter(source).Y < RectCenter(target).Y
+            ? intersectingRectangle.Height
+            : -intersectingRectangle.Height;
+        return new Vector2(0, d);
+      }
+    }
+
+    private static Vector2 RectCenter(RectangleF rect) {
+      return new Vector2(rect.X + rect.Width * 0.5f, rect.Y + rect.Height * 0.5f);
     }
     
-    private void RemoveCollisionActor(int entityId) {
-      if (_collisionActors.TryGetValue(entityId, out var actor)) {
-        _collisionActors.Remove(actor);
-        _collisionEngine.Remove(actor);
+    protected override void OnEntityAdded(int entityId) {
+      if (_positionComponentMapper.Has(entityId)
+          && _collisionComponentMapper.Has(entityId)
+          && !_motionComponentMapper.Has(entityId)) {
+        _collisionQuadTree.Add(new CollidableObject(
+            entityId,
+            _positionComponentMapper.Get(entityId),
+            _collisionComponentMapper.Get(entityId)));
       }
     }
-
-    private void AddCollisionActor(int entityId) {
-      var actor = new CollisionActor(
-          entityId,
-          _positionComponentMapper,
-          _collisionComponentMapper,
-          _motionComponentMapper);
-      _collisionActors.Add(actor);
-      _collisionEngine.Insert(actor);
+    
+    protected override void OnEntityChanged(int entityId) {
+      var dummy = new CollidableObject(entityId);
+      if (_collisionQuadTree.Contains(dummy)) {
+        _collisionQuadTree.Remove(dummy);
+      }
+      
+      OnEntityAdded(entityId);
+    }
+    
+    protected override void OnEntityRemoved(int entityId) {
+      _collisionQuadTree.Remove(new CollidableObject(entityId));
     }
   }
 
-  class CollisionActor : ICollisionActor {
-    // We store refs to the mappers instead of the components themselves, as in theory the
-    // components to any given entity could change at any time.
-    private readonly ComponentMapper<PositionComponent> _positionComponentMapper;
-    private readonly ComponentMapper<CollisionFootprintComponent>
-        _collisionFootprintComponentMapper;
-    private readonly ComponentMapper<MotionComponent> _motionComponentMapper;
+  public class CollidableObject : IRectFQuadStorable {
+    private readonly int _entityId;
+    public RectangleF Rect { get; }
 
-    // Allocate local bounds to prevent heap/garbage-collector churn from
-    // re-allocating rectangle struct every update, and provide cached bounds to accomodate
-    // multiple queries per update.
-    private RectangleF _bounds;
-
-    public int EntityId { get; }
-    public IShapeF Bounds => _bounds;
-
-    public CollisionActor(
-        int associatedEntity,
-        ComponentMapper<PositionComponent> positionComponentMapper,
-        ComponentMapper<CollisionFootprintComponent> collisionFootprintComponentMapper,
-        ComponentMapper<MotionComponent> motionComponentMapper) {
-      EntityId = associatedEntity;
-      _positionComponentMapper = positionComponentMapper;
-      _collisionFootprintComponentMapper = collisionFootprintComponentMapper;
-      _motionComponentMapper = motionComponentMapper;
-      _bounds = new RectangleF();
-      UpdateBounds();
+    /**
+     * Dummy that should only be used for #Contains calls 
+     */
+    public CollidableObject(int entityId) {
+      _entityId = entityId;
     }
     
-    public void OnCollision(CollisionEventArgs collisionInfo) {
-      var motionComponent = _motionComponentMapper.Get(EntityId);
-      // Only resolve collisions on entities with motion. Stationary entities aren't expected
-      // to move in response to collision.
-      if (motionComponent != null) { 
-        if (collisionInfo.PenetrationVector.Length() < motionComponent.CurrentMotion.Length()) {
-          // TODO: consider modifying direction against angled approaches to provide
-          // "walk around" effect.
-          motionComponent.CurrentMotion -= collisionInfo.PenetrationVector;
-        } else {
-          // Restrict correction to stationary to prevent bouncing effect.
-          motionComponent.CurrentMotion = Vector2.Zero;
-        }
-      }
+    public CollidableObject(
+        int entityId,
+        PositionComponent position,
+        CollisionFootprintComponent collisionFootprint) {
+      _entityId = entityId;
+      Rect = GetCollisionRect(position.Position, collisionFootprint);
     }
 
-    public void UpdateBounds() {
-      var currentMotion = _motionComponentMapper.Get(EntityId)?.CurrentMotion ?? Vector2.Zero;
-      var positionComponent = _positionComponentMapper.Get(EntityId);
-      var collisionFootprintComponent = _collisionFootprintComponentMapper.Get(EntityId);
-      // Apply not-yet-performed motion to bounds, to preempt collisions and correct them.
-      _bounds.X =
-          positionComponent.Position.X + collisionFootprintComponent.Footprint.X + currentMotion.X;
-      _bounds.Y =
-          positionComponent.Position.Y + collisionFootprintComponent.Footprint.Y + currentMotion.Y;
-      _bounds.Width = collisionFootprintComponent.Footprint.Width;
-      _bounds.Height = collisionFootprintComponent.Footprint.Height;
+    public static RectangleF GetCollisionRect(
+        Vector2 position, CollisionFootprintComponent collisionFootprint) {
+      return new RectangleF(
+          position.X + collisionFootprint.Footprint.Left,
+          position.Y + collisionFootprint.Footprint.Top,
+          collisionFootprint.Footprint.Width,
+          collisionFootprint.Footprint.Height);
+    }
+
+    /**
+     * Hashed based simply on EntityId, which is already a unique integer identifier
+     * of entities, for which there should only be one CollidableObject.
+     */
+    public override int GetHashCode() {
+      return _entityId;
     }
   }
 }
